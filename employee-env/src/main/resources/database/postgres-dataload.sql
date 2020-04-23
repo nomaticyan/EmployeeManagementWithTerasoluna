6e38c98776e9b078fab7375fe8f2a58a536a7758
nomaticyan <nomaticyan@gmail.com>
2020-04-23 10:29:57 +0630

    commit for log in screen

--- employee-env/src/main/resources/database/postgres-dataload.sql
+++ employee-env/src/main/resources/database/postgres-dataload.sql
@@ -1,2 +1,2 @@
-/* load the records. */
-commit;
+INSERT INTO account(username, password, first_name, last_name) VALUES('demo', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK', 'Taro', 'Yamada'); -- (1)
+COMMIT;
